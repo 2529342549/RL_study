e7d45be2881d4076682a9fc4bc393972587ce7cf
HHD <2529342549@qq.com>
2022-02-03 09:39:34 +0800

    new

--- torch_study/writer.py
+++ torch_study/writer.py
@@ -6,9 +6,18 @@
 @time: 2021/9/27 18:58
 @desc:
 '''
+import numpy
 from torch.utils.tensorboard import SummaryWriter
+from PIL import Image
+import numpy as np
 
 writer = SummaryWriter("logs")
+image_path="D:\\pythonProjects\\RL_study_\\torch_study\data\\train\\ants_image\\0013035.jpg"
+img_PIL=Image.open(image_path)
+img_array=numpy.array(img_PIL)
+# print(type(img_array))
+writer.add_image("test",img_array,1,dataformats='HWC')
+
 
 for i in range(100):
     writer.add_scalar("y=2x", 3 * i, i)
